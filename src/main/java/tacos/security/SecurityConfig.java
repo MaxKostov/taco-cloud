d4aed37def7e4c8b1a1163ca427cf79076fc1920
MaxKostov <max.kostov04@gmail.com>
2024-08-07 20:45:18 +0300

    Implemented authendification to the app

---
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package tacos.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import tacos.User;
+import tacos.data.UserRepository;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/design", "/orders").hasRole("USER")
+                                .requestMatchers("/", "/**").permitAll()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .permitAll()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/")
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+}
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,12 +2,17 @@ package tacos.web;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
+import tacos.User;
 import tacos.data.OrderRepository;
+import tacos.data.UserRepository;
+
+
 
 
 @Controller
@@ -15,10 +20,12 @@ import tacos.data.OrderRepository;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private final UserRepository userRepository;
     private OrderRepository orderRepo;
     @Autowired
-    public OrderController(OrderRepository orderRepo) {
+    public OrderController(OrderRepository orderRepo, UserRepository userRepository) {
         this.orderRepo = orderRepo;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/current")
@@ -27,11 +34,19 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
-
+        if (user != null) {
+            user.getAuthorities().forEach(authority ->
+                    System.out.println("Role: " + authority.getAuthority())
+            );
+        } else {
+            System.out.println("No authenticated user found");
+        }
+        order.setUser(user);
         orderRepo.save(order);
         System.out.println(order);
         sessionStatus.setComplete();
